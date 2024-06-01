b27a7113ab10f34221fadc78195aeee772086341
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-06-01 23:19:27 +0200

    changed pics

--- src/Fire.jsx
+++ src/Fire.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import fire from './photo/fire.gif'
+import fireM from './photo/fireM.gif'
+
 function Fire() {
     return (
         <div className='h-[10vh] flex w-full bg-black'>
-            <img className='w-1/4  opacity-80' src="/fireM.gif" alt="" />
-            <img className='w-1/4  opacity-80' src="/fire.gif" alt="" />
-            <img className='w-1/4  opacity-80' src="/fireM.gif" alt="" />
-            <img className='w-1/4  opacity-80' src="/fire.gif" alt="" />
+            <img className='w-1/4  opacity-80' src={fireM} alt="" />
+            <img className='w-1/4  opacity-80' src={fire} alt="" />
+            <img className='w-1/4  opacity-80' src={fireM} alt="" />
+            <img className='w-1/4  opacity-80' src={fire} alt="" />
         </div>
     );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import smoke1 from './photo/smoke1.gif'
+import smoke2 from './photo/smoke2.gif'
+import hero from './photo/hero.png'
+
 function Header() {
     return (
         <header className="h-[20vh] header text-orange-500 p-0 m-0">
@@ -7,13 +11,13 @@ function Header() {
                 <div className='absolute inset-0 flex justify-center items-center'>
                     <div className="h-full font-extrabold text-white text-9xl px-20 box-border flex items-center">
                         <p style={{ WebkitTextStroke: '4px orange', textStroke: '4px orange' }}>Dominant</p>
-                        <img className='w-32 h-32 mx-2' src="/hero.png" alt="" />
+                        <img className='w-32 h-32 mx-2' src={hero} alt="" />
                         <p className='' style={{ WebkitTextStroke: '4px orange', textStroke: '4px orange' }}>Food</p>
                     </div>
                 </div>
-                <img src="/smoke1.gif" alt="" srcset="" />
+                <img src={smoke1} alt="" srcset="" />
                 <div className='bg-black w-full'></div>
-                <img src="/smoke2.gif" alt="" srcset="" />
+                <img src={smoke2} alt="" srcset="" />
             </div>
         </header>
     );
