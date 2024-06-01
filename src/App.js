4f45ce5d9ba773a78fda05f0514693cd0f16e45d
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-06-01 02:37:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Fire from './Fire';
 import Header from './Header';
 import Orders from './Orders';
-import Admin from './Admin'; // Импортируем компонент Admin
 
 function App() {
 
-  const [orders, setOrders] = useState({ accepted: ["1", "2", "3"], ready: ["245"] });
+  const orders = { accepted: ["1", "2", "3"], ready: ["245"] }
 
   return (
     <div className='bg-black h-screen overflow-hidden'>
